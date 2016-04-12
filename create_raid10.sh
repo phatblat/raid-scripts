a3a24a9889cb97c5aee0d1dffa630d094f317d58
Ben Chatelain <ben@octop.ad>
2016-04-12 13:49:45 -0600

    Add create_raid10.sh script

---
+++ create_raid10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ex
+
+#
+# create_raid10.sh
+# Creates a RAID 1+0 (aka RAID 10) array using four disks.
+#
+
+# Create 2x mirrored sets (RAID 1)
+diskutil appleRAID create mirror Mirror1 JHFS+ disk3 disk4
+diskutil appleRAID create mirror Mirror2 JHFS+ disk6 disk7
+
+# Create stripe of the mirrored sets (RAID 0)
+diskutil appleRAID create stripe Mirror1 JHFS+ Mirror1 Mirror2
