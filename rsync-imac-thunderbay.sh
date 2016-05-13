4273c1ffb3cf3564d7b5a0295a2bca72ea7d3660
Ben Chatelain <ben@octop.ad>
2016-05-12 23:18:48 -0600

    Add rsync-imac-thunderbay.sh

---
+++ rsync-imac-thunderbay.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -ex
+
+#
+# rsync-imac-thunderbay.sh
+# Syncs files from iMac to external ThunderBay.
+#
+
+source=/Users/phatblat/
+destination=/Volumes/ThunderBay/Users/phatblat/
+
+echo "rsyncing ${source} -> ${destination}"
+mkdir -p "${destination}"
+
+# Test (-anv)
+rsync --archive --verbose --dry-run "${source}" "${destination}"
+
+# Run (-aP)
+# rsync --archive --partial --progress "${source}" "${destination}"
