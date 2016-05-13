8efd760db34bed8c76a01e374cc002e9bd7af1fb
Ben Chatelain <ben@octop.ad>
2016-05-12 23:00:04 -0600

    Update disk numbers and labels

--- create_raid0.sh
+++ create_raid0.sh
@@ -6,4 +6,4 @@
 #
 
 # Create stripe of the 4x drives (RAID 0)
-diskutil appleRAID create stripe RAID0Set JHFS+ disk3 disk4 disk6 disk7
+diskutil appleRAID create stripe ThunderBay JHFS+ disk3 disk5 disk6 disk7
--- create_raid10.sh
+++ create_raid10.sh
@@ -6,8 +6,8 @@
 #
 
 # Create 2x mirrored sets (RAID 1)
-diskutil appleRAID create mirror Mirror1 JHFS+ disk3 disk4
+diskutil appleRAID create mirror Mirror1 JHFS+ disk3 disk5
 diskutil appleRAID create mirror Mirror2 JHFS+ disk6 disk7
 
 # Create stripe of the mirrored sets (RAID 0)
-diskutil appleRAID create stripe RAID10Set JHFS+ Mirror1 Mirror2
+diskutil appleRAID create stripe ThunderBay JHFS+ Mirror1 Mirror2
